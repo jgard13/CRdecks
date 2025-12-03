13f4e998fa131a369bb0531557d4ae382fdb9d8b
jgard13 <jonathangaelgar@gmail.com>
2025-12-03 01:15:16 -0600

    Datos del formulario de registro hacia el back con POST

--- src/scripts/index_front.js
+++ src/scripts/index_front.js
@@ -14,3 +14,35 @@ togglePassword.addEventListener('click', function() {
         toggleIcon.classList.add('bi-eye');
     }
 });
+
+const Registro = document.getElementById('Registro');
+Registro.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('regUsuario').value;
+    const password = document.getElementById('regPassword').value;
+
+    const body = {
+        username,
+        password
+    };
+
+    try{
+        const res = await fetch('/registro', {
+            method: 'POST',
+            headers: {
+                "Content Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+
+        const data = await res.json();
+        alert(data.message);
+
+        if(res.ok){
+            window.location.href = "/index.html";
+        }
+    } catch(error){
+        console.error("Error:", error);
+    }
+});
