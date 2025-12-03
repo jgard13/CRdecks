356676480657d62ef6887199a5fad09e113aca46
jgard13 <jonathangaelgar@gmail.com>
2025-12-03 00:43:53 -0600

    Conexion a base

--- server/app.js
+++ server/app.js
@@ -1,18 +1,28 @@
 //modulos
 const express = require('express');
 const dotenv = require('dotenv');
+const mysql = require('mysql2');
 const path = require('path'); //necesito asignar rutas para mi servidor
 const cors = require('cors');
+const {config} = require('./config/config');
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
+//middleware para direcciones estaticas
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../src')));
 
 app.use(cors());
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    port: config.dbPort,
+})
 
 //Defino las rutas de inicio ahora si
 app.get('/', (req, res) => {
